23a2a3daeae241e76d23a147bba365a628b4194f
FALAKNAZMALICK <itsfalaknaz@gmail.com>
2026-04-17 20:51:13 +0500

    Add files via upload

---
+++ data_type tuple.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  8 15:15:49 2026
+
+@author: usama.khan
+"""
+"""
+
+TUPLE: 
+    collection of data stored under one variable
+    it can be of any data type
+    it is unchangeable / uneditable
+    it can be displayed
+    round brackets are used to initialize list ()
+    the data type inside a tuple can be inconsistent
+    
+    
+    initialization
+    access using index / print
+    length
+    join (+,*)
+    count
+    slicing
+
+
+"""
+
+t1 = (11,2,3,59,6,5,2)
+print(t1)
+
+## length of the tuple
+len_t1 = len(t1)
+print(len_t1) 
+
+# Index Ranges: Positive and negative (Slicing)
+t1[4:]
+
+t1[4:6]
+
+t1[-3:]
+
+t1[-3:-1]
+
+# Count
+t1.count(2)
+
+
+
+
+
+

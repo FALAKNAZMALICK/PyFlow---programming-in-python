cb6ea76d7d6f3727d2efdb1c1779de5fd62cce5b
FALAKNAZMALICK <itsfalaknaz@gmail.com>
2026-04-19 00:21:07 +0500

    Add files via upload

---
+++ data_type set.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  8 16:03:56 2026
+
+@author: usama.khan
+"""
+
+"""
+SET: 
+    collection of data stored under one variable
+    it can be of any data type
+    it is unchangeable , uneditable, unordered, unaccessible
+    it can be displayed
+    curly brackets are used to initialize set {}
+    the data type inside a tuple can be inconsistent
+    remove duplicates
+    
+    
+    
+    initialization
+    print
+    length
+    add/remove element (update, remove, pop, discard)
+    add/remove (set,tuple,list)
+
+"""
+
+s1 = {1,2,3,'a', 2, 66, 5}
+print(s1)
+
+## length of the set
+len_s1 = len(s1)
+print(len_s1) 
